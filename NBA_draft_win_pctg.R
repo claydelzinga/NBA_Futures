b4470e783e44646a5273915b65d56a8a8a49e887
claydelzinga <clay.elzinga@vinelandresearch.com>
2024-05-07 12:12:34 -0400

    Made a ggplot of the data

--- NBA_draft_win_pctg.R
+++ NBA_draft_win_pctg.R
@@ -3,14 +3,22 @@ library(rvest)
 library(dplyr)
 library(httr)
 library(stringr)
+library(ggplot2)
+library(ggrepel)
 
 
+#Web scraping using tables
+team_rankings_url <- "https://www.teamrankings.com/nba/stat/win-pct-all-games"
 
+rankings_table <- read_html(team_rankings_url, as.data.frame=T, stringsAsFactors = TRUE)
 
+rankings_table <- rankings_table %>% 
+  html_nodes("table") %>% 
+  .[[1]] %>% 
+  html_table(fill=T) 
+rankings_table
 
-
-
-
+#Web scrpaing using selector not table 
 extract_data <- function(url) {
   tryCatch({
     response <- httr::GET(url)
@@ -65,10 +73,6 @@ extract_data <- function(url) {
 
 url <- "https://therookiewire.usatoday.com/lists/2022-23-nba-team-by-team-future-draft-picks/"
 nba_team_data <- extract_data(url)
-nba_team_data
-
-
-
 
 ordered_draft_capital <- nba_team_data[order(nba_team_data$nba_team),]
 ordered_win_prcgt <- rankings_table[order(rankings_table$Team),]
@@ -78,5 +82,18 @@ NBA_draft_win <- cbind(ordered_draft_capital, ordered_win_prcgt)
 NBA_draft_win <- NBA_draft_win[c(1,4,7,12)]
 NBA_draft_win
 NBA_draft_win$total_picks <- as.numeric(str_extract(NBA_draft_win$total_picks, "-?\\d+"))
+NBA_draft_win <- rename(NBA_draft_win,
+                        draft_cap = 2,
+                        win_perc = 3,
+                        last_year = 4
+                        )
 NBA_draft_win
 
+
+ggplot(NBA_draft_win, aes(x = draft_cap, y = win_perc, label = nba_team)) +
+  geom_point(color = "skyblue", size = 2) +  
+  geom_text_repel(hjust = 0, vjust = 0, size = 1.75, color = "white") +  
+  labs(title = "Comparison of Teams Draft Capital with Win Percentage",
+       x = "Draft Capital",
+       y = "Win Percentage") +
+  theme_dark()
